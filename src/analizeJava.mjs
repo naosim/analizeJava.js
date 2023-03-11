df7710f7d01a4cf3ffefbf6bf89346eba76030a6
naosim <nao_pillows@yahoo.co.jp>
2023-03-11 14:28:04 +0900

    リソース配下のみ対象にする

--- src/analizeJava.mjs
+++ src/analizeJava.mjs
@@ -26,6 +26,7 @@ import { getJavaData } from "./logic.mjs";
 
 export function analizeJava(rootPath, isDDD) {
   return searchFiles(rootPath, ".java")
+  .filter(v => v.indexOf('/src/') != -1) // リソース配下のみ対象
   .filter(v => v.indexOf('/src/test/') == -1) // テストコードを除外
   .map((v) => ({ filePath: v, sourceCode: fs.readFileSync(v, "utf8") }))
   .map((v) => getJavaData(v, isDDD));
